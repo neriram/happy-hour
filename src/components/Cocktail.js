d0110921f04165f0482dafa0437137aa86fcafb8
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-12-15 22:24:31 -0800

    scaffolding components

---
+++ src/components/Cocktail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Cocktail = () => {
+    return (
+        <div>
+            <h2>Cocktail Component</h2>
+        </div>
+    )
+}
+
+export default Cocktail
